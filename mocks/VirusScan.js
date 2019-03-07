9317918371a82c366c95dafd9098c39c53cfd5f5
shepda <david@x-designs.co.uk>
2019-03-07 09:00:09 +0000

    Clean up virus scan mock

--- mocks/VirusScan.js
+++ mocks/VirusScan.js
@@ -1,9 +1,4 @@
-// const config = require('../src/config');
-
-// const {virusScan} = config.services;
-
 const mock = {
-  // path: `${virusScan.url}:${virusScan.port}${virusScan.path}`,
   path: '/scan',
   method: 'POST',
   template: {
